d50703945a4a61753377a02e96dcc74ef512e547
sya-ri <git@s7a.dev>
2022-04-16 12:58:59 +0900

    Fix apply-patch.sh

--- scripts/apply-patch.sh
+++ scripts/apply-patch.sh
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")" || return
 
 # SummerFields
-patch -d ../SummerFields < ../patches/SummerFields.patch
+patch -p1 -d ../SummerFields < ../patches/SummerFields.patch
 cd ../SummerFields || exit 1
 git add .
 git commit -m "Apply patch"
